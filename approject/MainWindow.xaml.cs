375f70c3484d3061898335121c647d7b437ad75d
hana-kr <hanakariman@yahoo.com>
2020-01-28 13:40:52 +0330

    submit

---
+++ approject/MainWindow.xaml.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.IO;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace approject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public List<people> People = new List<people>();
+        private void save(object sender, RoutedEventArgs e)
+        {
+            int sen;
+            string func1 = "";
+            string func2 = "";
+
+            List<string> str1 = new List<string>();
+            int lineCount = text.LineCount;
+            string str2 = text.Text;
+            string[] str3 = str2.Split(new string[] { Environment.NewLine },StringSplitOptions.RemoveEmptyEntries);
+            for(int i=0;i<str3.Length;i++)
+            {
+                str1.Add(str3[i]);
+            }
+            int j = 0;  
+            while (!int.TryParse(str1[j] , out sen)) {
+
+                 j++;
+                if (j == str1.Count)
+                {
+                    break;
+                }
+              }
+
+            for (int i = 0; i < str1.Count; i++)
+            {
+                if ((str1[i].Contains("=") && str1[i].Contains("+")) || (str1[i].Contains("=") && str1[i].Contains("-")))
+                {
+                    func1 = str1[i];
+                    
+                    
+                }
+                
+            }
+            for (int i = 0; i < str1.Count; i++)
+            {
+                if ( (str1[i].Contains("=")&& str1[i].Contains("+")) || (str1[i].Contains("=") && str1[i].Contains("-")))
+                {
+                    if (str1[i] == func1)
+                    {
+                       continue;
+                    }
+                    else
+                    {
+                        func2 = str1[i];
+
+                    }
+                }
+                
+                
+            }
+
+            people people = new people(str1[0], str1[1], sen, str1[2], func1, func2);
+            if (func1 == null || func2 == null)
+            {
+                people.funcanswer();
+            }
+            People.Add(people);
+           // People.Clear();
+            text.Text = String.Empty;
+            //text.Text += people.name;
+            //text.Text += people.familyname;
+            //text.Text += people.age+"\n";
+            //text.Text += people.city;
+            //text.Text += people.function1 + "\n";
+            //text.Text += people.function2;
+            //text.Text += people._x+"\n";
+            //text.Text += people._y;
+            if (People.Count != 0)
+            {
+
+                for (int f = 0; f < People.Count; f++)
+                {
+                    if ( str1[0] == People[f].name && str1[1] == People[f].familyname && sen == People[f].age && str1[2] == People[f].city && func1== People[f].function1 && func2 == People[f].function2)
+                    {
+                        break;
+                    }
+
+                }
+            }
+            StreamWriter sw = new StreamWriter("people.txt", true);
+            sw.WriteLine(people.name+"\n" + people.familyname+"\n" + people.age +"\n" + people.city + "\n" + people.function1 +"\n"+ people.function2);
+            sw.Close();
+
+        }
+
+        private void quit(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+
+        public List<string> boxes = new List<string>();
+        IEnumerable<string> result;
+
+        List<string> final = new List<string>();
+        List<string> final2 = new List<string>();
+        //List<string> many;
+        public List<string> before = new List<string>();
+        private void serch(object sender, RoutedEventArgs e)
+        {
+            resultcheck.Text = "";
+            before = new List<string>();
+            final = new List<string>();
+            final2 = new List<string>();
+            for (int i = 0; i < boxes.Count; i++)
+            {
+                if (boxes.Count == 0)
+                {
+                    resultcheck.Text = "";
+                    break;
+                }
+                
+                if (boxes[i] == "name")
+                {
+                    string first = namecheck.Text;
+               
+                    IEnumerable<string> result = People.Where(p => p.name == first).Select(p => p.name + " " + p.familyname);
+                    foreach(string output in result)
+                    {
+                        before.Add(output);
+                    }
+                }
+                if (boxes[i] == "familyname")
+                {
+                    string second = familycheck.Text;
+                    IEnumerable<string>  result = People.Where(p => p.familyname == second).Select(p => p.name + " " + p.familyname);
+                    foreach (string output in result)
+                    {
+                        before.Add(output);
+                    }
+                }
+                if (boxes[i] == "city")
+                {
+                    string third = citycheck.Text;
+                    IEnumerable<string> result = People.Where(p => p.city == third).Select(p => p.name + " " + p.familyname);
+                    foreach (string output in result)
+                    {
+                        before.Add(output);
+                    }
+                }
+                if (boxes[i] == "agemore")
+                {
+                    int four = int.Parse(agemorecheck.Text);
+                    IEnumerable<string> result = People.Where(p => p.age >= four).Select(p => p.name + " " + p.familyname);
+                    foreach (string output in result)
+                    {
+                        before.Add(output);
+                    }
+                }
+                if (boxes[i] == "ageless")
+                {
+                    int five = int.Parse(agelesscheck.Text);
+                    IEnumerable<string> result = People.Where(p => p.age <= five).Select(p => p.name + " " + p.familyname); 
+                    foreach (string output in result)
+                    {
+                        before.Add(output);
+                    }
+                }
+                if (boxes[i] == "function")
+                {
+                    string six = functioncheck.Text;
+                    IEnumerable<string> result = People.Where(p => p.function1 == six || p.function2 == six).Select(p => p.name + " " + p.familyname);
+                    foreach (string output in result)
+                    {
+                        before.Add(output);
+                    }
+                }
+                if (boxes[i] == "answers")
+                {
+                    int seven = int.Parse(answerscheck.Text);
+                    IEnumerable<string> result = People.Where(p => p._x == seven || p._y == seven).Select(p => p.name + " " + p.familyname);
+                    foreach (string output in result)
+                    {
+                        before.Add(output);
+                    }
+
+                }
+            }
+                
+                if (boxes.Count == 1)
+                {
+                resultcheck.Text = "";
+                foreach (string value in before)
+                {
+                    resultcheck.Text += value;
+                    resultcheck.Text += Environment.NewLine;
+                }
+
+                }
+                else if(boxes.Count>1)
+                {
+                for(int a=0; a<before.Count; a++)
+                {
+                    for(int b=0; b<before.Count; b++)
+                    {
+                        if (a == b)
+                        {
+                            continue;
+                        }
+                        if (before[a] == before[b])
+                        {
+                            final.Add(before[b]);
+                        }
+               
+                    }
+                }
+
+                foreach(string o in final)
+                {
+                    if (final2.Contains(o))
+                    {
+                        continue;
+                    }
+                    final2.Add(o);
+
+                }
+                resultcheck.Text = "";
+                foreach (string value in final2)
+                {
+                    resultcheck.Text += value;
+                    resultcheck.Text += Environment.NewLine;
+                }
+
+
+            }
+
+               
+            boxes = new List<string>();
+        }
+
+  
+
+       
+        private void checkbox(object sender, RoutedEventArgs e)
+        {
+            boxes.Add(((CheckBox)sender).Name);
+          
+        }
+
+       
+    }
+}
---
+++ approject/people.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace approject
+{
+    public class people
+    {
+        public string name;
+        public string familyname;
+        public int age;
+        public string city;
+        public string function1;
+        public string function2;
+        public double _x;
+        public double _y;
+
+
+        public people(string name , string familyname , int age , string city , string function1 , string function2)
+        {
+            this.name = name;
+            this.familyname = familyname;
+            this.city = city;
+            this.age = age;
+            this.function1 = function1;
+            this.function2 = function2;
+        }
+
+        int i=0;
+        char[] arr = new char[2];
+        public void funcanswer()
+        {
+            for (int x=0; x < function1.Length ; x++)
+            {
+                if (function1[x] == 'a' || function1[x] == 'b' || function1[x] == 'c' || function1[x] == 'd' || function1[x] == 'e' || function1[x] == 'f'
+                    || function1[x] == 'g' || function1[x] == 'h' || function1[x] == 'i' || function1[x] == 'j' || function1[x] == 'k' || function1[x] == 'l'
+                    || function1[x] == 'm' || function1[x] == 'n' || function1[x] == 'o' || function1[x] == 'p' || function1[x] == 'q' || function1[x] == 'r'
+                    || function1[x] == 's' || function1[x] == 't' || function1[x] == 'u' || function1[x] == 'v' || function1[x] == 'w' || function1[x] == 'x'
+                    || function1[x] == 'y' || function1[x] == 'z')
+                {
+                    arr[i] = function1[x];
+                    i++;
+                }
+            }
+            List<int> Zarayeb1 = new List<int>();
+            string[] x1 = function1.Split(arr[0]);
+
+            if (x1[0] == "")
+            {
+                Zarayeb1.Add(1);
+            }
+            else
+            {
+                Zarayeb1.Add(int.Parse(x1[0]));
+            }
+            string[] y = x1[1].Split(arr[1]);
+            string str = "";
+           // for(int t=1;t<y[0].Length;t++)
+            //{
+              //  str += y[0][t];
+            //}
+            Zarayeb1.Add(int.Parse(y[0]));
+
+            string[] str2;
+            str2 = function1.Split('=');
+
+            Zarayeb1.Add(int.Parse(str2[str2.Length - 1]));
+
+            i = 0;
+            for (int x = 0; x < function2.Length; x++)
+            {
+                if (function2[x] == 'a' || function2[x] == 'b' || function2[x] == 'c' || function2[x] == 'd' || function2[x] == 'e' || function2[x] == 'f'
+                    || function2[x] == 'g' || function2[x] == 'h' || function2[x] == 'i' || function2[x] == 'j' || function2[x] == 'k' || function2[x] == 'l'
+                    || function2[x] == 'm' || function2[x] == 'n' || function2[x] == 'o' || function2[x] == 'p' || function2[x] == 'q' || function2[x] == 'r'
+                    || function2[x] == 's' || function2[x] == 't' || function2[x] == 'u' || function2[x] == 'v' || function2[x] == 'w' || function2[x] == 'x'
+                    || function2[x] == 'y' || function2[x] == 'z')
+                {
+                    arr[i] = function2[x];
+                    i++;
+                }
+            }
+            List<int> Zarayeb2 = new List<int>();
+             x1 = function2.Split(arr[0]);
+
+            if (x1[0] == "")
+            {
+                Zarayeb2.Add(1);
+            }
+            else
+            {
+                Zarayeb2.Add(int.Parse(x1[0]));
+            }
+             y = x1[1].Split(arr[1]);
+           
+            str = "";
+           // for (int t = 1; t < y[0].Length; t++)
+            //{
+              //  str += y[0][t];
+           // }
+            Zarayeb2.Add(int.Parse(y[0]));
+
+        
+            str2 = function2.Split('=');
+
+            Zarayeb2.Add(int.Parse(str2[str2.Length - 1]));
+
+            int a = Zarayeb1[0]; int b = Zarayeb1[1]; int c = -Zarayeb1[2];
+            
+            int p = Zarayeb2[0]; int q = Zarayeb2[1]; int r = -Zarayeb2[2];
+            try
+            {
+                _x = (r * a - c * p) / (a * q - b * p);
+                _y = (b * r - c * q) / (a * q - b * p);
+
+
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Solution not possible", e);
+                
+            }
+        }
+    }
+}
